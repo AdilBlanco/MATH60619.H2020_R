00ef912abd3a7b9bc74ba3082f09c70b6e2fd111
Adil Labiad <adillabiad@gmail.com>
2020-02-08 21:18:28 -0500

    devoir-1 q8.

--- devoir-1/devoir-1.R
+++ devoir-1/devoir-1.R
@@ -182,3 +182,169 @@ t.test(prix ~ flag, data=df7, var.equal=F, alternative='greater')
 # en fin de semaine est plus chère que les jours de la semiane à niveau de confiance de 5%.
 
 #******************************************************************************
+df8 <- df[(df$type=="AVE" | df$type=="AVE-TGV") & (df$tarif=="Promo"), ]
+df8$weekend <- with(df8, ifelse(jour %in% c(6, 7), 1, 0))
+head(df8, n=5)
+#     prix    type     classe tarif dest duree jour weekend
+# 1 143.40     AVE Preferente Promo    0   190    6       1
+# 3  86.80     AVE Preferente Promo    0   165    7       1
+# 4  86.80     AVE Preferente Promo    0   190    7       1
+# 5  68.95 AVE-TGV Preferente Promo    0   175    4       0
+# 6  53.20     AVE Preferente Promo    0   190    7       1
+
+# (8-a)
+# prix = β0 + β1.dest + β2.classe + β3.duree + β4.weekend + E
+
+# (8-b)
+class(df8$classe)
+df8$classe <- factor(df8$classe)
+mod <- lm(prix ~ dest + classe + duree + weekend, data=df8)
+summary(mod)
+# Residuals:
+#   Min      1Q    Median      3Q     Max 
+# -53.473  -7.450   1.841   9.225  68.610 
+# Coefficients:
+#                   Estimate Std. Error   t value    Pr(>|t|)    
+# (Intercept)       135.341077   1.708457  79.218   <2e-16 ***
+#   dest              0.433242   0.298252   1.453    0.146    
+# classeTurista     -17.764789   0.532803 -33.342   <2e-16 ***
+# classeTuristaPlus  -6.927631   0.587443 -11.793   <2e-16 ***
+# classeTuristaSolo  -9.069268   9.471460  -0.958    0.338    
+# duree              -0.231672   0.009464 -24.480   <2e-16 ***
+# weekend            -0.408880   0.347892  -1.175    0.240    
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+# Residual standard error: 13.37 on 8052 degrees of freedom
+# Multiple R-squared:  0.206,	Adjusted R-squared:  0.2054 
+# F-statistic: 348.1 on 6 and 8052 DF,  p-value: < 2.2e-16
+
+# prix_^ = 135.34 + 0.44.dest_^ + ((-17.76) + (-6.93) + (-9.07)).classe_^ + (-0.23).duree_^ 
+#          + (-0.41).weekend_^
+
+# dest (binaire): En fixant toutes les autres co-variables, toutes choses étant égales, 
+#                 le prix moyen du trajet Madrid->Barcelone augmente de 0.44 par rapport 
+#                 au trajet Barcelone->Madrid.
+# duree (continue): En fixant toutes les autres co-variables, toutes choses étant égales, 
+#                   une augmentation de la duree d'une minute implique une dimunition de 0.41 minutes
+#                   du prix en moyenne.
+# weekend (binaire): En fixant toutes les autres co-variables, toutes choses étant égales,
+#                    le prix moyen en fin de semaine diminue de 0.41 par rappot au jour semaine.
+#
+#
+# # (8-c)
+#***************
+# Destination  #
+#***************
+# H0: β1=0
+# H1: β1#0
+mod1 <- lm(prix ~ dest, data=df8)
+summary(mod1)
+# Residuals:
+#  Min      1Q     Median      3Q     Max 
+# -50.260  -7.160   2.540   8.385  87.085 
+# Coefficients:
+#           Estimate Std. Error    t value   Pr(>|t|)    
+# (Intercept)  82.1145     0.2373  345.967   <2e-16 ***
+#   dest        0.4454     0.3342    1.333    0.183    
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# Residual standard error: 15 on 8057 degrees of freedom
+# Multiple R-squared:  0.0002204,	Adjusted R-squared:  9.628e-05 
+# F-statistic: 1.776 on 1 and 8057 DF,  p-value: 0.1827
+
+# On ne rejete pas H0, donc il n'y a pas d'effet significatif de la destination sur le prix.
+#***********
+#  classe  #
+#***********
+# H0: β2=0
+# H1: β2#0
+mod2 <- lm(prix ~ classe, data=df8)
+summary(mod2)
+# Residuals:
+#    Min      1Q   Median      3Q     Max 
+# -49.391  -8.041   2.309   6.479  73.838 
+# Coefficients:
+#                  Estimate Std.   Error   t value   Pr(>|t|)    
+#   (Intercept)        95.3616     0.5184  183.944   <2e-16 ***
+#   classeTurista     -16.7407     0.5514  -30.361   <2e-16 ***
+#   classeTuristaPlus  -7.1706     0.6095  -11.764   <2e-16 ***
+#   classeTuristaSolo  -7.4616     9.8297  -0.759     0.448    
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# Residual standard error: 13.88 on 8055 degrees of freedom
+# Multiple R-squared:  0.1441,	Adjusted R-squared:  0.1438 
+# F-statistic: 452.2 on 3 and 8055 DF,  p-value: < 2.2e-16
+!!!!!!!!!!!!
+#***********
+#   Duree  #
+#***********
+# H0: β3=0
+# H1: β3#0
+mod3 <- lm(prix ~ duree, data=df8)
+summary(mod3)
+# Residuals:
+#     Min      1Q  Median      3Q     Max 
+# -47.392  -7.897   1.803   9.726  83.199 
+# Coefficients:
+#           Estimate Std.  Error t   value   Pr(>|t|)    
+# (Intercept) 113.03652    1.74146   64.91   <2e-16 ***
+#   duree     -0.18024     0.01018  -17.71   <2e-16 ***
+#  ---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+# Residual standard error: 14.72 on 8057 degrees of freedom
+# Multiple R-squared:  0.03745,	Adjusted R-squared:  0.03733 
+# F-statistic: 313.5 on 1 and 8057 DF,  p-value: < 2.2e-16
+
+# On rejete H0, donc il y a un effet significatif de la duree sur le prix.
+#***********
+# weekend  #
+#***********
+# H0: β4=0
+# H1: β4#0
+mod4 <- lm(prix ~ weekend, data=df8)
+summary(mod4)
+# Residuals:
+#     Min      1Q    Median      3Q     Max 
+#   -50.435  -7.335   2.365   7.765  86.465 
+# Coefficients:
+#            Estimate Std. Error   t value  Pr(>|t|)    
+# (Intercept)  82.7349     0.1929  428.938  < 2e-16 ***
+#   weekend    -1.5779     0.3852   -4.097 4.23e-05 ***
+#   ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# Residual standard error: 14.99 on 8057 degrees of freedom
+# Multiple R-squared:  0.002079,	Adjusted R-squared:  0.001955 
+# F-statistic: 16.78 on 1 and 8057 DF,  p-value: 4.233e-05
+
+# On rejete H0, donc il y a un effet significatif de la fin de semaine sur le prix.
+#*************
+# Le modele  #
+#*************
+mod <- lm(prix ~ dest + classe + duree + weekend, data=df8)
+summary(mod)
+# Residuals:
+#   Min      1Q    Median      3Q     Max 
+# -53.473  -7.450   1.841   9.225  68.610 
+# Coefficients:
+#                   Estimate Std.  Error    t value   Pr(>|t|)    
+#   (Intercept)       135.341077   1.708457  79.218   <2e-16 ***
+#   dest                0.433242   0.298252   1.453    0.146    
+#   classeTurista     -17.764789   0.532803 -33.342   <2e-16 ***
+#   classeTuristaPlus  -6.927631   0.587443 -11.793   <2e-16 ***
+#   classeTuristaSolo  -9.069268   9.471460  -0.958    0.338    
+#   duree              -0.231672   0.009464 -24.480   <2e-16 ***
+#   weekend            -0.408880   0.347892  -1.175    0.240    
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# Residual standard error: 13.37 on 8052 degrees of freedom
+# Multiple R-squared:  0.206,	Adjusted R-squared:  0.2054 
+# F-statistic: 348.1 on 6 and 8052 DF,  p-value: < 2.2e-16
+
+par(mfrow = c(2,2)) 
+plot(mod)
+
+
+
+# prix = β0 + β1.dest + β2.classe + β3.duree + β4.weekend + E
