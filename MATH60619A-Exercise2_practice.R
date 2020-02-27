d416654c9503257575350690ed46eb237b52ea93
Adil Labiad <adillabiad@gmail.com>
2020-02-26 20:53:04 -0500

    hypothesis testing update questions exercise 2.

--- MATH60619A-Exercise2_practice.R
+++ MATH60619A-Exercise2_practice.R
@@ -318,6 +318,15 @@ summary(assurance)
 # + region: lieu de résidence, une région parmi sudouest, sudest, nordouest ou nordest,
 # + frais: les frais médicaux annuels en 2013 (en dollars USD).
 
+# Classification        IMC (kg/m2)
+#-------------------------------------------------
+# < 18.5              Insuffisance pondérale
+# 18.5 – 24.9         Corpulence normale
+# 25.0 – 29.9         Surpoids
+# 30.0 – 34.9         Obésité
+# 35.0 – 39.9         Obésité de classe II et III
+#-------------------------------------------------
+
 # a)
 colnames(assurance)
 # [1] "age"    "sexe"   "imc"    "enfant" "fumeur" "region" "frais"
@@ -384,14 +393,6 @@ legend(x="topleft",
        pch=c(19,4,1),
        cex = 0.5)
 
-# Classification        IMC (kg/m2)
-#-------------------------------------------------
-# < 18.5              Insuffisance pondérale
-# 18.5 – 24.9         Corpulence normale
-# 25.0 – 29.9         Surpoids
-# 30.0 – 34.9         Obésité
-# 35.0 – 39.9         Obésité de classe II et III
-#-------------------------------------------------
 par(mfrow=c(1, 1))
 assurance$classe <- cut(assurance$imc, breaks = c(0, 18.5, 25, 30, 35, max(assurance$imc)),
                         labels=c("<18.5", "18.5-24.9", "25.0-29.9", "30.0-34.9", "35.0-39.9"), include.lowest=TRUE)
@@ -405,18 +406,295 @@ legend(x="topleft",
        col=c("#333333", "#FFCC00", "#FF3300", "#99CC00", "#3399FF"),
        pch=c(19, 4, 1, 22, 23, 24),
        cex = 0.5)
+# b)
+# H0: μ_fum = μ_non
+# Ha: μ_fum <> μ_non
+# ou μ_fum est la moyenne des frais médicaux payés par les fumeurs
+# et μ_non est la moyenne des frais médicaux payés par non fumeurs.
 
+par(mfrow=c(3,2), pch=20, bty='l')
+# histogram plot fee for smokers.
+hist(assurance$frais[assurance$fumeur=='oui'], freq=FALSE, col="lightblue",
+     xlab="fee", main="Smoker", ylab="density")
+lines(density(assurance$frais[assurance$fumeur=='oui']), lty=2,col="black", lwd=1)
+lines(curve(dnorm(x, mean= mean(assurance$frais[assurance$fumeur=='oui']), 
+                  sd=sd(assurance$frais[assurance$fumeur=='oui'])), from=20, to=200, add=TRUE), col="red", lwd=2)
+# histogram plot fee for no smokers.
+hist(assurance$frais[assurance$fumeur=='non'], freq=FALSE, col="lightblue",
+     xlab="fee", main="No smoker", ylab="density")
+lines(density(assurance$frais[assurance$fumeur=='non']), lty=2,col="black", lwd=1)
+lines(curve(dnorm(x, mean= mean(assurance$frais[assurance$fumeur=='non']), 
+                  sd=sd(assurance$frais[assurance$fumeur=='non'])), from=20, to=200, add=TRUE), col="red", lwd=2)
+# Normal Quantile-Q plot fee for smokers.
+qqnorm(assurance$frais[assurance$fumeur=='oui'], pch=1, main="Smoker", frame=FALSE)
+qqline(assurance$frais[assurance$fumeur=='oui'], col="steelblue", lwd=2)
+# Normal Quantile-Q plot fee for no smokers.
+qqnorm(assurance$frais[assurance$fumeur=='non'], pch=1, main="No smoker", frame=FALSE)
+qqline(assurance$frais[assurance$fumeur=='non'], col="steelblue", lwd=2)
+# Box plot offer
+boxplot(assurance$frais ~ assurance$fumeur, 
+        horizontal=TRUE, 
+        col="lightblue", 
+        xlab="fee", 
+        ylab="smoker",
+        main="fee per group",
+        frame=FALSE)
 
+var.test(frais ~ fumeur, data=assurance)
+# 	F test to compare two variances
+# data:  frais by fumeur
+# F = 0.2697, num df = 1063, denom df = 273, p-value < 2.2e-16
+# alternative hypothesis: true ratio of variances is not equal to 1
+# 95 percent confidence interval:
+#   0.2222063 0.3238772
+# sample estimates:
+#   ratio of variances 
+#   0.2696967 
 
+# La valeur de valeur-p provenant du test de variance est de 2.2e-16 < 0.05, 
+# donc on rejette H0, alors les variances dans les deux groupes sont 
+# significativement différentes. Donc pour faire un test d'égalité de moyenne
+# le choix est porté sur un test non paramétrique (Fligner-Policello)
 
+RVAideMemoire::fp.test(frais ~ fumeur, data=assurance, alternative="two.sided")
+# 	Fligner-Policello test
+# data:  frais by fumeur
+# U* = 139.41, p-value < 2.2e-16
+# alternative hypothesis: true difference in location is not equal to 0
 
+# La valeur de valeur-p provenante du test Plicello est de 2.2e-16 < 0.05,
+# on rejette H0, alors les deux moyennes sont significativement différentes.
 
+# c)
+# H0: μ_fob = μ_fnob
+# Ha: μ_fob > μ_fnob
+# ou μ_fob est la moyenne des frais des fumeurs et obèse et
+# μ_fnob est la moyenne des frais des fumeurs et non obèse.
 
+# fummeur & obèses
+# assurance$frais[(assurance$fumeur=="oui") & (assurance$imc >= 30)]
+# fumeur & non obèses
+# assurance$frais[(assurance$fumeur=="oui") & (assurance$imc < 30)]
+assurance$is_obese <- with(assurance, ifelse(imc>=30, 1, 0))
+head(assurance, n=4)
+#     age sexe    imc enfant fumeur region    frais class     classe    is_obese
+# 1    19 femme  27.9      0 oui    sudouest  16885 25.0-29.9 25.0-29.9        0
+# 2    18 homme  33.8      1 non    sudest     1726 30.0-34.9 30.0-34.9        1
+# 3    28 homme  33        3 non    sudest     4449 30.0-34.9 30.0-34.9        1
+# 4    33 homme  22.7      0 non    nordouest 21984 18.5-24.9 18.5-24.9        0
 
+par(mfrow=c(3,2), pch=20, bty='l')
+# histogram plot fee for smokers & obese.
+hist(assurance$frais[(assurance$fumeur=="oui") & (assurance$imc>=30)], freq=FALSE, col="lightblue",
+     xlab="fee", main="Smoker obese.", ylab="density")
+lines(density(assurance$frais[(assurance$fumeur=="oui") & (assurance$imc>=30)]), lty=2,col="black", lwd=1)
+lines(curve(dnorm(x, mean= mean(assurance$frais[(assurance$fumeur=="oui") & (assurance$imc>=30)]), 
+                  sd=sd(assurance$frais[(assurance$fumeur=="oui") & (assurance$imc>=30)])), from=20, to=200, add=TRUE), col="red", lwd=2)
+# histogram plot fee for smokers no obese.
+hist(assurance$frais[(assurance$fumeur=="oui") & (assurance$imc<30)], freq=FALSE, col="lightblue",
+     xlab="fee", main="Smoker no obese.", ylab="density")
+lines(density(assurance$frais[(assurance$fumeur=="oui") & (assurance$imc<30)]), lty=2,col="black", lwd=1)
+lines(curve(dnorm(x, mean= mean(assurance$frais[(assurance$fumeur=="oui") & (assurance$imc < 30)]), 
+                  sd=sd(assurance$frais[(assurance$fumeur=="oui") & (assurance$imc<30)])), from=20, to=200, add=TRUE), col="red", lwd=2)
+# Normal Quantile-Q plot fee for smokers obese.
+qqnorm(assurance$frais[(assurance$fumeur=="oui") & (assurance$imc>=30)], pch=1, main="Smoker obese.", frame=FALSE)
+qqline(assurance$frais[(assurance$fumeur=="oui") & (assurance$imc>=30)], col="steelblue", lwd=2)
+# Normal Quantile-Q plot fee for smokers no obese.
+qqnorm(assurance$frais[(assurance$fumeur=="oui") & (assurance$imc<30)], pch=1, main="Smoker no obese.", frame=FALSE)
+qqline(assurance$frais[(assurance$fumeur=="oui") & (assurance$imc<30)], col="steelblue", lwd=2)
+# Box plot offer
+boxplot(assurance$frais[assurance$fumeur=="oui"] ~ assurance$is_obese[assurance$fumeur=="oui"], 
+        horizontal=TRUE, 
+        col="lightblue", 
+        xlab="fee", 
+        ylab="smoker & is_obese",
+        main="fee per group",
+        frame=FALSE)
 
+smoker_df <- as.data.frame(assurance[assurance$fumeur=="oui", ])
+var.test(frais ~ is_obese, data=smoker_df)
+# 	F test to compare two variances
+# data:  frais by is_obese
+# F = 0.7057, num df = 128, denom df = 144, p-value = 0.04444
+# alternative hypothesis: true ratio of variances is not equal to 1
+# 95 percent confidence interval:
+#   0.5040902 0.9913143
+# sample estimates:
+#   ratio of variances 
+# 0.7057005 
 
+t.test(frais ~ is_obese, 
+       data=smoker_df, 
+       var.eq=FALSE, 
+       alternative="greater", 
+       conf.level=.95)
+# Welch Two Sample t-test
+# data:  frais by is_obese
+# t = -30.108, df = 271.13, p-value = 1
+# alternative hypothesis: true difference in means is greater than 0
+# 95 percent confidence interval:
+#   -21301.83       Inf
+# sample estimates:
+#   mean in group 0 mean in group 1 
+#         21363.21        41557.99 
 
+# La valeur-p provenante du test de Welch est de 1 > 0.05, donc
+# on ne rejette pas H0.
 
+t.test(frais ~ is_obese, 
+       data=smoker_df, 
+       var.eq=FALSE, 
+       alternative="greater", 
+       conf.level=.9)
+# 	Welch Two Sample t-test
+# data:  frais by is_obese
+# t = -30.108, df = 271.13, p-value = 1
+# alternative hypothesis: true difference in means is greater than 0
+# 90 percent confidence interval:
+#   -21056.47       Inf
+# sample estimates:
+#  mean in group 0 mean in group 1 
+#         21363.21        41557.99 
 
+t.test(frais ~ is_obese, 
+       data=smoker_df, 
+       var.eq=FALSE, 
+       alternative="greater", 
+       conf.level=.99)
+# 	Welch Two Sample t-test
+# data:  frais by is_obese
+# t = -30.108, df = 271.13, p-value = 1
+# alternative hypothesis: true difference in means is greater than 0
+# 99 percent confidence interval:
+#   -21764.43       Inf
+# sample estimates:
+#   mean in group 0 mean in group 1 
+#          21363.21        41557.99 
 
+# d)
+par(mfrow=c(3,2), pch=20, bty='l')
+# histogram plot imc vs women.
+hist(assurance$imc[assurance$sexe=="femme"], freq=FALSE, col="lightblue",
+     xlab="fee", main="Women vs IMC.", ylab="density")
+lines(density(assurance$imc[assurance$sexe=="femme"]), lty=2,col="black", lwd=1)
+lines(curve(dnorm(x, mean= mean(assurance$imc[assurance$sexe=="femme"]), 
+                  sd=sd(assurance$imc[assurance$sexe=="femme"])), from=20, to=200, add=TRUE), 
+      col="red", lwd=2)
+# histogram plot imc vs men.
+hist(assurance$imc[assurance$sexe=="homme"], freq=FALSE, col="lightblue",
+     xlab="fee", main="Men vs IMC.", ylab="density")
+lines(density(assurance$imc[assurance$sexe=="homme"]), lty=2,col="black", lwd=1)
+lines(curve(dnorm(x, mean= mean(assurance$imc[assurance$sexe=="homme"]),
+                  sd=sd(assurance$imc[assurance$sexe=="homme"])), from=20, to=200, add=TRUE), 
+      col="red", lwd=2)
+# Normal Quantile-Q plot imc vs Women.
+qqnorm(assurance$imc[assurance$sexe=="femme"], pch=1, main="Women vs IMC.", frame=FALSE)
+qqline(assurance$imc[assurance$sexe=="femme"], col="steelblue", lwd=2)
+# Normal Quantile-Q plot imc vs men.
+qqnorm(assurance$imc[assurance$sexe=="homme"], pch=1, main="Men vs IMC.", frame=FALSE)
+qqline(assurance$imc[assurance$sexe=="homme"], col="steelblue", lwd=2)
+# Box plot offer
+boxplot(assurance$imc ~ assurance$sexe, 
+        horizontal=TRUE, 
+        col="lightblue", 
+        xlab="imc", 
+        ylab="smoker & is_obese",
+        main="IMC per sexe",
+        frame=FALSE)
+
+# H0: μ_imcm = μ_imcw
+# Ha: μ_imcm <> μ_imcw
+# ou μ_imcm est la moyenne de l'indice de masse corporelle chez les hommes et
+# μ_imcw est la moyenne de l'indice de masse corporelle chez les femmes.
+
+var.test(imc ~ sexe, data=assurance)
+# 	F test to compare two variances
+# data:  imc by sexe
+# F = 0.96967, num df = 661, denom df = 675, p-value = 0.691
+# alternative hypothesis: true ratio of variances is not equal to 1
+# 95 percent confidence interval:
+#   0.8331483 1.1286986
+# sample estimates:
+#   ratio of variances 
+#  0.9696692 
+
+t.test(imc ~ sexe, data=assurance, var.eq=FALSE)
+# 	Welch Two Sample t-test
+# data:  imc by sexe
+# t = -1.6974, df = 1336, p-value = 0.08986
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   -1.21910025  0.08808432
+# sample estimates:
+#   mean in group femme mean in group homme 
+#           30.37976            30.94527 
+
+# La valeur-p provenante du test de Welch est de 0.08986 > 0.05, on ne rejette
+# pas H0. Alors on a pas assez de preuves pour conclure qu'il n'y a une différence
+# entre entre l'indice de masse corporelle chez les hommes et les femmes.
+
+# e)
+# H0: μ_imcn = μ_imcs
+# Ha: μ_imcn <> μ_imcs
+# ou μ_imcn est la moyenne de l'indice corporelle chez les habitants du Nord
+# et μ_imcs est la moyenne de l'indice corporelle chez les habitants du Sud.
+
+assurance$direction <- with(assurance, ifelse(region %in% c("sudouest", "sudest" ), "S", "N"))
+
+par(mfrow=c(3,2), pch=20, bty='l')
+# histogram plot imc vs direction Nord
+hist(assurance$imc[assurance$direction=="N"], freq=FALSE, col="lightblue",
+     xlab="fee", main="North vs IMC.", ylab="density")
+lines(density(assurance$imc[assurance$direction=="N"]), lty=2,col="black", lwd=1)
+lines(curve(dnorm(x, mean= mean(assurance$imc[assurance$direction=="N"]), 
+                  sd=sd(assurance$imc[assurance$direction=="N"])), from=20, to=200, add=TRUE), 
+      col="red", lwd=2)
+# histogram plot imc vs direction Sud
+hist(assurance$imc[assurance$direction=="S"], freq=FALSE, col="lightblue",
+     xlab="fee", main="South vs IMC.", ylab="density")
+lines(density(assurance$imc[assurance$direction=="S"]), lty=2,col="black", lwd=1)
+lines(curve(dnorm(x, mean= mean(assurance$imc[assurance$direction=="S"]),
+                  sd=sd(assurance$imc[assurance$direction=="S"])), from=20, to=200, add=TRUE), 
+      col="red", lwd=2)
+# Normal Quantile-Q plot imc vs direction Nord
+qqnorm(assurance$imc[assurance$direction=="N"], pch=1, main="North vs IMC.", frame=FALSE)
+qqline(assurance$imc[assurance$direction=="N"], col="steelblue", lwd=2)
+# Normal Quantile-Q plot imc vs direction Sud.
+qqnorm(assurance$imc[assurance$direction=="S"], pch=1, main="South vs IMC.", frame=FALSE)
+qqline(assurance$imc[assurance$direction=="S"], col="steelblue", lwd=2)
+# Box plot offer
+boxplot(assurance$imc ~ assurance$direction, 
+        horizontal=TRUE, 
+        col="lightblue", 
+        xlab="imc", 
+        ylab="Direction",
+        main="IMC per direction",
+        frame=FALSE)
+
+var.test(imc ~ direction ,data=assurance)
+# 	F test to compare two variances
+# data:  imc by direction
+# F = 0.78249, num df = 648, denom df = 688, p-value = 0.001586
+# alternative hypothesis: true ratio of variances is not equal to 1
+# 95 percent confidence interval:
+#   0.6723606 0.9109823
+# sample estimates:
+#   ratio of variances 
+# 0.7824924 
+
+t.test(imc ~ direction, data=assurance, var.eq=FALSE)
+# 	Welch Two Sample t-test
+# data:  imc by direction
+# t = -8.8715, df = 1330.8, p-value < 2.2e-16
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   -3.501108 -2.233108
+# sample estimates:
+#   mean in group N mean in group S 
+#        29.18906        32.05617 
+
+# La valeur-p provenante du test de Welch est de 2.2e-16 < 0.05, 
+# donc on rejette H0, on peut conclure qu'il y a une différence 
+# de moyenne entre l'indice de masse corporelle chez les habitants
+# du Nord et les habitants du Sud.
 
